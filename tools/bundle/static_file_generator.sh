19a0b4516aa06444a4b6db7477cd307dcfb4af0a
Nate Sulat <nates@nates.local>
2020-10-20 13:34:06 -0700

    hotfix for a missing directory

--- tools/bundle/static_file_generator.sh
+++ tools/bundle/static_file_generator.sh
@@ -4,6 +4,12 @@ CONFIG_FILE=$1
 BUILD_TYPE=$2
 
 # Because this is run from package.json, filepaths stem from the root directory
+if [ ! -d "./src/assets" ]; then
+    echo "Creating src/assets directory..."
+    mkdir ./src/assets
+    echo "Done!\n"
+fi
+
 if [ ! -d "./src/assets/images" ]; then
     echo "Creating src/assets/images directory..."
     mkdir ./src/assets/images
